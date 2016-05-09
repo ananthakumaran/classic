ebd7dcf705bc249ebdfd1ed8ea9604fc468195fb
Anantha Kumaran <ananthakumaran@gmail.com>
2016-05-09 10:06:09 +0530

    add tooltip

--- site/source/javascripts/all.js
+++ site/source/javascripts/all.js
@@ -1,2 +1,8 @@
-window.jQuery = require("../../bower_components/jquery/dist/jquery.js");
+var $ = window.jQuery = require("../../bower_components/jquery/dist/jquery.js");
 require("../../bower_components/bootstrap/dist/js/umd/dropdown.js");
+window.Tether = require('tether');
+require("../../bower_components/bootstrap/dist/js/umd/tooltip.js");
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
